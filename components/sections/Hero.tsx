9c7df77793d4573796f68ba21b5fc0530840652a
Chilufya <chilufya@nodedropp.com>
2025-01-09 12:25:49 -0600

    fixing linting errors

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+// import Link from 'next/link'
 import React from 'react'
 import PortableTextBody from '../PortableTextBody';
 import { myPortableTextComponents } from '../PortableTextComponents';
