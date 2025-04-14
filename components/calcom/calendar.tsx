f07dd97fc67f1d826ec4fb7b42cffb2a48c0e47f
Chilufya <chilufya@nodedropp.com>
2025-04-13 22:49:47 -0500

    fixing broken cal.com embedd

--- components/calcom/calendar.tsx
+++ components/calcom/calendar.tsx
@@ -6,22 +6,17 @@ import { useEffect } from "react";
 const notebook = {
     text: `Schedule a free 15-minute consultation to walk through your ideas, challenges, and the best next steps—custom to your business.`
 }
-export default function MyApp() {
+
+export default function Calendar() {
     useEffect(() => {
         (async function () {
-            const cal = await getCalApi({ "namespace": "15min" });
-            cal("ui", { "theme": "light", "cssVarsPerTheme": { "light": { "cal-brand": "#fafafa" }, "dark": { "cal-brand": "#292929" } }, "hideEventTypeDetails": false, "layout": "month_view" });
+            const cal = await getCalApi({ "namespace": "nodedropp" });
+            cal("ui", { "hideEventTypeDetails": false, "layout": "month_view" });
         })();
     }, [])
-
-    return (
-        <>
-            <p className="font-semibold text-center mx-auto text-[length:var(--text-1)] max-w-[60ch] ">{notebook.text}</p>
-            <Cal namespace="15min"
-                calLink="cmmutale/15min"
-                style={{ width: "100%", height: "100%", overflow: "scroll" }}
-                config={{ "layout": "month_view", "theme": "light" }}
-            />
-        </>
-    );
+    return <Cal namespace="nodedropp"
+        calLink="cmmutale/nodedropp"
+        style={{ width: "100%", height: "100%", overflow: "scroll" }}
+        config={{ "layout": "month_view" }}
+    />;
 };
