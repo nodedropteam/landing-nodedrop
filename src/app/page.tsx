7e94065414c0fe2d87f5fb18f67647c0fa05c39a
Chilufya <chilufya@nodedropp.com>
2025-05-13 10:11:20 -0500

    restarting project

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <div className='page-wrapper full '>
+            <div className="page-content">
+                <section>
+                    <div className="copy">
+                        <h1 className='font-bold text-[clamp(3.5rem,10vw,10rem)] leading-[100%] p-2'>
+                            websites and digital tools that are unique like
+                            your business.
+                        </h1>
+                        {/* <p>
+                            We deliver custom software solutions, crafted
+                            with care and a local-first mindset, to simplify your
+                            operations, reach more customers, and build lasting
+                            success in your community.
+                        </p> */}
+                    </div>
+                </section>
+            </div>
+        </div>
+    )
+}
